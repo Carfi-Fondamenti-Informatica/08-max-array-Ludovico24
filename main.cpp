573e5cbdefea40f1af89010e8254eb31b228184e
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-11 17:30:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 using namespace std;
 
 int main(){
-    int dim=0;
-    cin>>dim;
-    float a[dim];
-    for(int i=0; i<dim; i++){
-        cin>>a[i];
-    }
-    cout<<array(a,dim)<<endl;
-    return 0;
+  
+  int n = 0;
+  cin >> n;
+  
+  
+ float array[n];
+ 
+ for(int i=0;i<n;i++){
+ cin >> array[i];
+ }
+  
+  cout << massimo(array,n) << endl;
+  
+  return 0;
 }
