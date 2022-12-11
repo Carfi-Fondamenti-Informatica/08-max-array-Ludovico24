7796aecc5e321bcc55dbb7c738fba1957db412e9
Ludovico24 <117870218+Ludovico24@users.noreply.github.com>
2022-12-11 17:22:01 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
+#Includi "lib.h"
+float maxArray(float array[], int d){
+    for(int i = 0; i < d; ++i){
+        if(array[0] > array[i]){
 
+        }
+        altro{
+            array[0] = array[i];
+        }
+    }
+    matrice restituita[0];
+}
